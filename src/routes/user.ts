d7447dd4d98a26bb1c52aa15a3faea2de8694513
Boris Antonijev <antonijevboris@gmail.com>
2023-01-16 13:59:34 +0100

    Update user.ts

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,29 +4,22 @@ import { UserController } from "../modules/user/user.controller";
 import { isLoggedIn } from "../modules/auth/strategy/passport";
 
 export function userRoutes(app: any): Application {
-  app.group("/api/v1", (router: any) => {
-    router.group("/user", (subRouter: any) => {
-      // Subroutes under user
-      subRouter.get(
-        "/personal-info",
-        isLoggedIn,
-        (req: Request, res: Response) => {
-          UserController.personalInfo(req, res);
-        }
-      );
-      subRouter.post(
-        "/change-password",
-        isLoggedIn,
-        (req: Request, res: Response) => {
-          UserController.changePassword(req, res);
-        }
-      );
-      subRouter.post("/forgot-password", (req: Request, res: Response) => {
-        UserController.forgotPassword(req, res);
-      });
-      subRouter.post("/reset-password/:id", (req: Request, res: Response) => {
-        UserController.resetPassword(req, res);
-      });
+  app.group("/api/v1/user", (router: any) => {
+    router.get("/personal-info", isLoggedIn, (req: Request, res: Response) => {
+      UserController.personalInfo(req, res);
+    });
+    router.post(
+      "/change-password",
+      isLoggedIn,
+      (req: Request, res: Response) => {
+        UserController.changePassword(req, res);
+      }
+    );
+    router.post("/forgot-password", (req: Request, res: Response) => {
+      UserController.forgotPassword(req, res);
+    });
+    router.post("/reset-password/:id", (req: Request, res: Response) => {
+      UserController.resetPassword(req, res);
     });
   });
 
