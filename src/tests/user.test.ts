a2798301cbe8c3f941cc7a504695f74234246809
Boris Antonijev <antonijevboris@gmail.com>
2023-01-19 12:21:53 +0100

    Update user.test.ts

--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -5,19 +5,48 @@ import { Request, Response } from "express";
 
 const mockRequest = (body: any): Request => {
   return {
-    body: { data: body },
+    body: body,
   } as Request;
 };
 
 const mockResponse = () => {
   const res: any = {};
-  res.status = jest.fn().mockReturnValue(res);
-  res.send = jest.fn().mockReturnValue(res);
-  res.json = jest.fn().mockReturnValue(res);
+  res.status = jest.fn().mockReturnThis();
+  res.send = jest.fn().mockReturnThis();
   return res;
 };
 
 describe("register", () => {
+  test("should return 201 if user is registered", async () => {
+    const mockDataRegister: any = {
+      id: 5,
+      email: "fake@fake.com",
+      password: "fake12345",
+      firstName: "Fake",
+      lastName: "Fake",
+      phone: "123123",
+      address: "Fake 17",
+      profilePicture: "Fake",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    const user: any = {
+      email: "fake@fake.com",
+      password: "fake12345",
+      confirmPassword: "fake12345",
+      firstName: "Fake",
+      lastName: "Fake",
+      phone: "123123",
+      address: "Fake 17",
+      profilePicture: "Fake",
+    };
+    const req = mockRequest(user);
+    const res = mockResponse();
+    prismaMock.user.create.mockResolvedValue(mockDataRegister);
+    await AuthController.register(req, res);
+    expect(res.status).toHaveBeenCalledWith(201);
+    //expect(res.send).toHaveBeenCalledWith({});
+  });
   test("should return 401 if data in not set", async () => {
     const req = mockRequest({});
     const res = mockResponse();
